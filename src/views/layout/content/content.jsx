65a2abc73caef9d99e5a3ccbc50cdc850c8ddecb
56-djalmajr <djalmajr@gmail.com>
2016-11-02 22:52:04 -0300

    Ajustes na pagina de busca

--- src/views/layout/content/content.jsx
+++ src/views/layout/content/content.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import moment from "moment";
 import faker from "faker";
 import times from "lodash/times";
-import {Card, Icon, Image} from "semantic-ui-react";
+import {Card, Rating, Image} from "semantic-ui-react";
 import FlexColumn from "views/components/flex-column";
 import FlexRow from "views/components/flex-row";
 import styles from "./content.scss";
@@ -14,7 +14,7 @@ class Content extends React.Component {
         const records = times(19).map(() => ({
             "title": faker.name.findName(),
             "meta": "350 visualizações",
-            "description": faker.hacker.phrase(),
+            "description": faker.lorem.sentences(3, 3),
             "image": faker.internet.avatar(),
             "created_at": new Date().toJSON(),
         }));
@@ -34,13 +34,20 @@ class Content extends React.Component {
                                 <Image floated="left" size="mini" src={record.image} />
                                 <Card.Header>{record.title}</Card.Header>
                                 <Card.Meta>{record.meta}</Card.Meta>
-                                <Card.Description>{record.description}</Card.Description>
+                                <Card.Description>
+                                    {record.description.length - 3 > 100 ?
+                                        record.description.slice(0, 100) + "..." :
+                                        record.description
+                                    }
+                                </Card.Description>
                             </Card.Content>
                             <Card.Content extra>
-                                <FlexRow justify="space-between">
-                                    <a href="#"><Icon name="heart" /></a>
-                                    <span>{moment(record.created_at).fromNow()}</span>
-                                </FlexRow>
+                                <FlexColumn align="flex-end">
+                                    <Rating icon="heart" rating={3} maxRating={5} disabled />
+                                    <span style={{marginTop: "0.5em"}}>
+                                        {moment(record.created_at).fromNow()}
+                                    </span>
+                                </FlexColumn>
                             </Card.Content>
                         </Card>
                     )}
